1752a05e12378a5c36366fe3f16af25b6cff4aac
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-05-16 10:36:17 +0700

    connect component with state global

--- src/container/pages/Product/Cardproduct/Counter.js
+++ src/container/pages/Product/Cardproduct/Counter.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react';  
+// connect merupakan suatu hoc yang akan di panggil di export 
+import {connect} from 'react-redux';
 class Counter extends Component {
 
     // ketika terdapat perubahan maka di a akan melakukan render 
     // ulang state yang terdapat di dalam component  
     // jika menggunakan variable biasa maka akan error karena variable biasa tidak dapat di definisikan dalam class 
-    state = {
-        order: 4
-    }
+    // state = {
+    //     order: 4
+    // }
 
     // memberi nilai dari props yang di terima dari product 
     handleCounterChange = (newState) => {
@@ -38,18 +40,33 @@ class Counter extends Component {
 
     }
 
-    render() {
+    render() { 
+        console.log(this.props);
         return ( 
             // membuat counter menjadi component tersendiri  
             <div className="counter">
                 <center>
                     <button className="minus" onClick={this.handleMinus}>-</button>
-                    {/* make default value 3 */}
-                    <input type="text" value={this.state.order}></input>
+                    {/* make default value 3 */} 
+                    {/* redux value global  */}
+                    <input type="text" value={this.props.order}></input>
                     <button className="plus" onClick={this.handlePluss}>+</button>
                 </center>
             </div>
         )
     }
 }
-export default Counter;
+
+// mengambil state global lalu di ubah menjadi props 
+const mapStateToProps = (state) =>{  
+    // mengambil totalOrder dari state global 
+    return {  
+        // mengubah total order menjadi order 
+        order: state.totalOrder
+    }
+}
+
+// params pertama dari connect adalah state global, yang kedua adalah dispatch dari redux 
+// connect component dengan redux
+// maka props global yang di inisilisasi akan di panggil 
+export default connect(mapStateToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker'; 
-import Home from './container/Home/Home';
+import Home from './container/Home/Home'; 
+// import create store 
+import {createStore} from 'redux'; 
+// penyedia store global  
+import {Provider} from 'react-redux';
+
+const globalState = { 
+  totalOrder: 0
+} 
+
+// 1. Reducer 
+// globalState memberi initial state awal 
+const rootReducer = (state = globalState, action) =>{ 
+  return state;
+}
+
+// 2. store
+const storeRedux = createStore(rootReducer);
 
 // melakukan render compoennt 
-ReactDOM.render(
-  <Home/>,
+ReactDOM.render( 
+  // tempat inisial store 
+ <Provider store={storeRedux}>
+    <Home/>
+ </Provider> ,
   document.getElementById('root')
 );
 
